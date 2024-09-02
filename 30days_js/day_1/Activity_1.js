2d0524eab2378c9ce8341c2d224fee4ef87f71a7
Omkarwebdev <omkarfakeac123@gmail.com>
2024-09-02 20:18:59 +0530

    commit 30daysjs

---
+++ 30days_js/day_1/Activity_1.js
@@ -0,0 +1,68 @@
+// Activity 1: Variable Declaration
+
+// task1
+var no=1
+console.log(no);
+
+// task2
+let name="omkar"
+console.log(name);
+
+
+// Activity 2: Constant Declaration
+
+// task3
+const user=true
+console.log(user);
+
+// task4
+let  login= ["omkar",2,true]
+let obj={
+    name:"omkar",
+    id:5,
+    login:true
+}
+
+console.log(typeof(no));
+console.log(typeof(name));
+console.log(typeof(user));
+console.log(typeof(login));
+console.log(typeof(obj));   
+
+//Activity 5: Understanding cont
+
+// task5
+user=false
+console.log(user);
+// error is because when will be decalred a value in const it will not change 
+
+
+// Feature Request:
+
+// task1
+let u1="omkar"
+console.log(u1);
+
+function myfunction(){
+    console.log("hello world!");
+    
+}
+console.log(myfunction());
+console.log(typeof(myfunction));
+
+
+// task2
+let u="omkar"
+console.log(u);
+
+user="sai"
+console.log(user);
+
+
+const c=1
+console.log(c);
+
+c=2
+console.log(c);
+
+// error: Assignment to constant variable.
